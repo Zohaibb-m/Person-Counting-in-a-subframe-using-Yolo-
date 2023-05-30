892ecbcaedb7c1d06c73188833e3196acf0109f2
Zohaib <zohaibmunir32@gmail.com>
2023-05-30 18:30:29 +0500

    Fixed OCR Accuracy

--- demo_date/demo_date.py
+++ demo_date/demo_date.py
@@ -1,4 +1,6 @@
 import argparse
+import time
+
 import cv2
 import numpy as np
 import pytesseract
@@ -14,10 +16,10 @@ vertices = [
         (38, 109)
 
     ], [
-        (101, 70),
-        (154, 70),
-        (154,  107),
-        (101, 107)
+        (103, 70),
+        (148, 70),
+        (148, 107),
+        (103, 107)
     ],
     [
         (170, 70),
@@ -25,7 +27,7 @@ vertices = [
         (255, 107),
         (170, 107)
     ],
-    #[     (272, 70),
+    # [     (272, 70),
     #     (346, 70),
     #     (346, 107),
     #     (272, 107)
@@ -36,10 +38,10 @@ vertices = [
         (408, 105),
         (408, 73)
     ], [
-        (424, 70),
-        (472, 70),
-        (472, 107),
-        (424, 107)
+        (428, 70),
+        (478, 70),
+        (478, 107),
+        (428, 107)
     ], [
         (493, 70),
         (540, 70),
@@ -55,8 +57,8 @@ vertices_full_date = [
     (35, 107)
 ]
 
-# coordinates = [[35, 70, 84, 107], [101, 70, 149, 107], [170, 70, 259, 107], [272, 70, 346, 107], [361, 70, 411, 107],
-#                [424, 70, 472, 107], [493, 70, 540, 107]]
+coordinates = [[35, 70, 84, 107], [101, 70, 149, 103], [170, 70, 259, 107], [272, 70, 346, 107], [361, 70, 411, 107],
+               [427, 70, 478, 107], [493, 70, 544, 103]]
 
 weekdays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 reader = Reader(['en'], True)
@@ -69,27 +71,66 @@ day = False
 month = False
 year = False
 date_found = False
+save_counter = 0
+
+import copy
 
 
-def find_date(frame):
+def detect_easy_ocr(frame):
+    global save_counter
+    result = reader.readtext(frame, allowlist='0123456789')
+    for (bbox, text, prob) in result:
+        if text is not None:
+            print(text)
+            return int(text)
+    print('Preprocessing')
+    frame_new = preprocess_image(copy.deepcopy(frame))
+    result = reader.readtext(frame_new, allowlist='0123456789')
+    for (bbox, text, prob) in result:
+        if text is not None:
+            return int(text)
+    print("Preprocessing 2")
+    frame_mix = preprocess_image_mix(copy.deepcopy(frame))
+    result = reader.readtext(frame_mix, allowlist='0123456789')
+    for (bbox, text, prob) in result:
+        if text is not None:
+            return int(text)
+    cv2.imwrite(f'./failed_images/{save_counter}_failed_image.jpg', frame)
+    cv2.imwrite(f'./failed_images/{save_counter}_failed_image_mix.jpg', frame_mix)
+    cv2.imwrite(f'./failed_images/{save_counter}_failed_image_new.jpg', frame_new)
+    save_counter += 1
+    return False
+
+
+def find_date(frame, crop, show):
     global date, month, year, day, date_found
     if not date:
-        cropped_frame = crop_frame(frame, vertices[1])
-        result = reader.readtext(cropped_frame, allowlist='0123456789')
-        for (bbox, text, prob) in result:
-            if 31 >= int(text) > 0:
-                date = int(text)
+        if crop:
+            cropped_frame = get_cropped_image(frame, coordinates[1])
+        else:
+            cropped_frame = crop_frame(frame, vertices[1])
+        if show:
+            cv2.imshow('date', cropped_frame)
+        result = detect_easy_ocr(cropped_frame)
+        if 31 >= result > 0:
+            date = result
     if not month:
-        cropped_frame = crop_frame(frame, vertices[0])
-        # cropped_frame = pre_process(cropped_frame)
-        # cv2.imshow('fr', cropped_frame)
-        result = reader.readtext(cropped_frame, allowlist='0123456789')
-        for (bbox, text, prob) in result:
-            # print(f'Result{text}')
-            if 12 >= int(text) > 0:
-                month = int(text)
+        if crop:
+            cropped_frame = get_cropped_image(frame, coordinates[0])
+        else:
+            cropped_frame = crop_frame(frame, vertices[0])
+        if show:
+            cv2.imshow('month', cropped_frame)
+        result = detect_easy_ocr(cropped_frame)
+        if 12 >= result > 0:
+            month = result
     if not year:
-        cropped_frame = crop_frame(frame, vertices[2])
+        if crop:
+            cropped_frame = get_cropped_image(frame, coordinates[2])
+        else:
+            cropped_frame = crop_frame(frame, vertices[2])
+        if show:
+            cv2.imshow('year', cropped_frame)
         result = reader.readtext(cropped_frame, allowlist='0123456789')
         for (bbox, text, prob) in result:
             if len(text) == 4:
@@ -103,20 +144,73 @@ def find_date(frame):
 
     if date_found:
         return f'{month}-{date}-{year}-{day}'
+    return month, date, year, day
+
+
+def get_hour(frame, crop, show):
+    if crop:
+        cropped_frame = get_cropped_image(frame, coordinates[4])
+    else:
+        cropped_frame = crop_frame(frame, vertices[3])
+    if show:
+        cv2.imshow('hour', cropped_frame)
+    result = detect_easy_ocr(cropped_frame)
+    if 23 >= result >= 0:
+        return result
     return False
 
 
-def get_hour(frame):
-    cropped_frame = crop_frame(frame, vertices[3])
-    cv2.imshow('fr', cropped_frame)
-    result = reader.readtext(cropped_frame, allowlist='0123456789')
-    for (bbox, text, prob) in result:
-        if 23 >= int(text) >= 0:
-            return int(text)
+def get_min(frame, crop, show):
+    if crop:
+        cropped_frame = get_cropped_image(frame, coordinates[5])
+    else:
+        cropped_frame = crop_frame(frame, vertices[4])
+    if show:
+        cv2.imshow('min', cropped_frame)
+    result = detect_easy_ocr(cropped_frame)
+    if result:
+        if 60 >= result >= 0:
+            return result
     return False
+
+
+def get_second(frame, crop, show):
+    if crop:
+        cropped_frame = get_cropped_image(frame, coordinates[6])
+    else:
+        cropped_frame = crop_frame(frame, vertices[5])
+    if show:
+        cv2.imshow('sec', cropped_frame)
+    result = detect_easy_ocr(cropped_frame)
+    if result:
+        if 60 >= result >= 0:
+            return result
+    return False
+
+
+def preprocess_image_mix(image):
+    val = 230
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # Create a black canvas according to the image size
+    canvas = np.zeros_like(gray)
+
+    # Invert the image
+    inverted = cv2.bitwise_not(gray)
+
+    # Find white pixels with intensity value greater than 245 in the original image
+    original_white_pixels = np.where(gray > val)
+    # Find white pixels with intensity value greater than 245 in the inverted image
+    inverted_white_pixels = np.where(inverted > val)
+
+    # Place white pixels onto the black canvas
+    canvas[original_white_pixels] = 255
+    canvas[inverted_white_pixels] = 255
+
+    return canvas
+
 
 def get_cropped_image(frame, coordinate):
-    print(coordinate)
+    # print(coordinate)
     return frame[coordinate[1]:coordinate[3], coordinate[0]: coordinate[2]]
 
 
@@ -133,6 +227,22 @@ def pre_process(frame):
     return thr
 
 
+def preprocess_image(image):
+    # Convert the image to grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # Invert the image
+    inverted = cv2.bitwise_not(gray)
+
+    # Apply adaptive histogram equalization
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+    equalized = clahe.apply(inverted)
+
+    # cv2.imshow("inverted", inverted)
+    # cv2.imshow("equalized", equalized)
+
+    return equalized
+
+
 def crop_frame(frame, vertices):
     mask = np.zeros_like(frame)
     roi_corners = np.array([vertices], dtype=np.int32)
@@ -140,8 +250,8 @@ def crop_frame(frame, vertices):
     return cv2.bitwise_and(frame, mask)
 
 
-def find_date_through_easyocr(frame):
-    print(find_date(frame))
+def find_date_through_easyocr(frame, crop, show):
+    print(find_date(frame, crop, show))
 
 
 def find_date_thorugh_tessaract(frame):
@@ -159,51 +269,52 @@ def find_date_thorugh_tessaract(frame):
         cv2.imshow(f'thr{i}', images[i])
 
 
-def run(args):
+def run(crop, show):
     # Open video file
-    cap = cv2.VideoCapture(args.source)
-    # Get video dimensions
-    # width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    # height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))q
-    fps = int(cap.get(cv2.CAP_PROP_FPS))
-    i = 0
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
-        # cv2.resize(frame, (720, 1080))
-        global hour, mint, sec
-        if i % fps == 0:
-            sec += 1
-            if sec == 60:
-                sec = 0
-                mint += 1
-                if mint == 60:
-                    mint = 0
-                    hour += 1
-        if i % int(int(fps) / int(fps / 2)) == 0:
-            find_date_through_easyocr(frame)
-            print(get_hour(frame))
-        i += 1
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    # Release resources
-    cap.release()
-    # out.release()
-    cv2.destroyAllWindows()
+    file = open('Logs.csv', 'w')
+    for inn in range(1):
+        print('Hi')
+        # cap = cv2.VideoCapture(f'../../cctv_ger/ch03_0{inn + 1}.mp4')
+        cap = cv2.VideoCapture(f'../sample_videos/sample4.mp4')
+        # Get video dimensions
+        # width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        # height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))q
+        fps = int(cap.get(cv2.CAP_PROP_FPS))
+        i = 0
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                break
+            # cv2.resize(frame, (720, 1080))
+            global hour, mint, sec
+            if i % fps == 0:
+                sec += 1
+                if sec == 60:
+                    sec = 0
+                    mint += 1
+                    if mint == 60:
+                        mint = 0
+                        hour += 1
+            if i % int(int(fps) / int(fps / 2)) == 0:
+                find_date_through_easyocr(frame, crop, show)
+                # file.write(f'{month},{date},{year},{day},')
+                # file.write(f'{get_hour(frame)},{get_min(frame)},{get_second(frame)}\n')
+                print(f'Hour: {get_hour(frame, crop, show)}')
+                print(f'Min: {get_min(frame, crop, show)}')
+                print(f'Sec: {get_second(frame, crop, show)}')
+            i += 1
+            if i % 1000 == 0:
+                print(i)
+            # cv2.waitKey(0)
+            # time.sleep(4)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        # Release resources
+        cap.release()
+        # out.release()
+        cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
-    # filenames = ['ch02_20230330090000.mp4', 'ch02_20230330113656.mp4', 'ch02_20230330162911.mp4' ,
-    #               'ch02_20230330165845.mp4', 'ch02_20230330211521.mp4']
-    filenames = ['ch02_20230330165845.mp4']
-    for file in filenames:
-        parser = argparse.ArgumentParser()
-        # parser.add_argument('--source', type=str, default=f'../../cctv_ger/{file}', help='path to video file')
-        parser.add_argument('--source', type=str, default=f'../sample_videos/video2.mp4', help='path to video file')
-        parser.add_argument('--weights', nargs='+', type=str,
-                            default=r"/home/kamran/tkbees/Project_Person_Counting/yolov5/Yolov5l_Fine_Tuned.pt",
-                            help='path to YOLO weights file')
-        opt = parser.parse_args()
-        os.environ['TESSDATA_PREFIX'] = '/home/kamran/miniconda3/envs/yolov8/share/'
-        run(opt)
+    os.environ['TESSDATA_PREFIX'] = '/home/kamran/miniconda3/envs/yolov8/share/'
+    run(crop=True, show=True)
